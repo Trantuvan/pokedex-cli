7f6bb8b48e0c57eb81ba05794a5faae01132c91e
trantuvan <van.trantu@posco.net>
2024-09-30 15:15:18 +0700

    get pokemons name in location area and caching

--- internal/pokeapi/poke_api.go
+++ internal/pokeapi/poke_api.go
@@ -52,3 +52,44 @@ func (c Client) GetLocationAreasPaginated(url string) locationArea {
 
 	return *locationAreas
 }
+
+func (c Client) ExplorePokemonInArea(url string) pokemonInArea {
+	pokemonInArea := &pokemonInArea{}
+
+	if pokemons, ok := c.cache.Get(url); ok {
+		errUnmarshal := json.Unmarshal(pokemons, pokemonInArea)
+
+		if errUnmarshal != nil {
+			log.Fatal(errUnmarshal)
+		}
+
+		return *pokemonInArea
+	}
+
+	res, errGet := c.httpClient.Get(url)
+
+	if errGet != nil {
+		log.Fatal(errGet)
+	}
+
+	defer res.Body.Close()
+	body, errRead := io.ReadAll(res.Body)
+
+	// * filter response only show res with status code 2**
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody:%s\n", res.StatusCode, body)
+	}
+
+	if errRead != nil {
+		log.Fatal(errRead)
+	}
+
+	c.cache.Add(url, body)
+	errUnmarshal := json.Unmarshal(body, pokemonInArea)
+
+	if errUnmarshal != nil {
+		log.Fatal(errUnmarshal)
+	}
+
+	return *pokemonInArea
+}
