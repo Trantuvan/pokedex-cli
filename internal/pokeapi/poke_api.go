912b63009116da9ec3bf46a447df10f96773e7f4
trantuvan <trantuvan.kan@gmail.com>
2024-10-02 21:41:40 +0700

    calling pokemon info and calc percentage caught pokemon

--- internal/pokeapi/poke_api.go
+++ internal/pokeapi/poke_api.go
@@ -4,12 +4,18 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"math"
+	"math/rand"
 )
 
 const (
-	BaseURL      = "https://pokeapi.co/api/v2/"
-	LocationArea = "location-area/"
+	BaseURL               = "https://pokeapi.co/api/v2/"
+	LocationArea          = "location-area/"
+	maximumBaseExperience = 635
+	minimumBaseExperince  = 40
 )
+
+var Pokemons map[string]catchPokemon
 
 func (c Client) GetLocationAreasPaginated(url string) locationArea {
 	locationAreas := &locationArea{}
@@ -92,4 +98,45 @@ func (c Client) ExplorePokemonInArea(url string) pokemonInArea {
 	}
 
 	return *pokemonInArea
+}
+
+func (c Client) Catch(url string) map[string]catchPokemon {
+	Pokemons = map[string]catchPokemon{}
+	catch := &catchPokemon{}
+	res, errGet := c.httpClient.Get(url)
+
+	if errGet != nil {
+		log.Fatal(errGet)
+	}
+
+	defer res.Body.Close()
+	body, errRead := io.ReadAll(res.Body)
+
+	// * filter response only show res with status code 2**
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody:%s\n", res.StatusCode, body)
+	}
+
+	if errRead != nil {
+		log.Fatal(errRead)
+	}
+
+	errUnmarshal := json.Unmarshal(body, catch)
+
+	if errUnmarshal != nil {
+		log.Fatal(errUnmarshal)
+	}
+
+	if percent := calcCatchPercentage(catch.BaseExperience); percent >= 30 {
+		Pokemons[catch.Name] = *catch
+	}
+
+	return Pokemons
+}
+
+func calcCatchPercentage(baseExperience float64) float64 {
+	percent := (1 - (baseExperience-minimumBaseExperince)/(maximumBaseExperience-minimumBaseExperince)) * 100
+	randomEffect := rand.Intn(100)
+	catchPercent := percent + float64(randomEffect)
+	return math.Min(catchPercent, 100)
 }
